63ebd703681fcf9e7ea002d487355d2262b54d6f
Sebati HM <sebashermon003@gmail.com>
2025-05-12 01:15:23 -0600

    Credentials DELETE added

--- src/routes/handlers/profile_handler.rs
+++ src/routes/handlers/profile_handler.rs
@@ -1,18 +1,34 @@
 use actix_web::{delete, web, HttpMessage, HttpRequest, HttpResponse};
-use sea_orm::{EntityTrait, ModelTrait};
+use sea_orm::{ActiveModelTrait, ActiveValue::Set, ConnectionTrait, DatabaseBackend, EntityTrait, ModelTrait, Statement};
 
 use crate::{routes::handlers::dto_model::account_dto::Message, utils::{app_state::AppState, jwt::Claims}};
 
 
 #[delete("")]
 pub async fn delete_account(req: HttpRequest, app_state: web::Data<AppState>) -> HttpResponse {
-    let account = entity::credentials::Entity::find_by_id(req.extensions().get::<Claims>().unwrap().id).one(&app_state.db).await.unwrap().unwrap();
-    let result= account.delete(&app_state.db).await;
+    let db = &app_state.db;
+
+    // 🔻 Desactivar constraints
+    let _ = db.execute(Statement::from_string(DatabaseBackend::MySql, "SET FOREIGN_KEY_CHECKS = 0".to_owned())).await;
+
+    let account = entity::credentials::Entity::find_by_id(req.extensions().get::<Claims>().unwrap().id)
+        .one(db)
+        .await
+        .unwrap()
+        .unwrap();
+
+    
+    let result = account.delete(db).await;
+
+    // 🔺 Reactivar constraints
+    let _ = db.execute(Statement::from_string(DatabaseBackend::MySql, "SET FOREIGN_KEY_CHECKS = 1".to_owned())).await;
 
     match result {
-        Ok(_) => return HttpResponse::Ok().json(Message{message: format!("Borrado correctamente") }),
-        Err(err) => return HttpResponse::InternalServerError().json(Message{ message: format!("Something went wrong {err}") }),
+        Ok(_) => HttpResponse::NoContent().json(Message {
+            message: "Account Deleted successfully".to_string(),
+        }),
+        Err(err) => HttpResponse::InternalServerError().json(Message {
+            message: format!("Something went wrong {err}"),
+        }),
     }
-    
-
 }
